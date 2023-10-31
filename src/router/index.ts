672ea052bb4ef063bd7a6dbf09f0d3c03c0d6369
jssatorres <juansataz.dev@gmail.com>
2023-10-31 09:13:48 +0100

    feat: error explanation and screenshots

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeViewvue'
 
+// si lo hago con @ da fallo me encuntra las carpetas cuando navega con la / pero no encutras los archivos de vue
+// los archvios de css si los encuentra he dejado capturas de pantalla en la carpeta screenshot
+// import HomeView from '../views/HomeViewvue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
